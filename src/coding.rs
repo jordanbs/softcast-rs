3beed8c976546e249ae3e5c2b3a569e36eef88da
Jordan Schneider <jbschne@umich.edu>
2025-12-10 14:13:11 -0800

    Implement the chunking iterator.

--- src/coding.rs
+++ src/coding.rs
@@ -46,6 +46,62 @@ impl<const LENGTH: usize, PixelType: HasPixelComponentType>
     }
 }
 
+fn max_factor_at_or_below(limit: usize, value: usize) -> usize {
+    assert!(limit > 0);
+    (1..=limit).rev().find(|i| value % i == 0).unwrap()
+}
+
+impl TransformBlock3DDCT {
+    pub fn chunks_iter(&mut self) -> impl Iterator<Item = ChunkedTransformBlock3D<'_>> {
+        const SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS: (usize, usize, usize) = (1, 44, 30);
+        let (length, width, height) = self.values.dim();
+        let chunk_length = max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.0, length);
+        let chunk_width = max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.1, width);
+        let chunk_height = max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.2, height);
+
+        let chunk_size = chunk_width * chunk_height * chunk_length;
+        assert!(chunk_size > 0);
+        assert_eq!((length * width * height) % chunk_size, 0);
+        let num_chunks = (length * width * height) / chunk_size;
+
+        let chunk_dimensions = (chunk_length, chunk_width, chunk_height);
+
+        // must preflight mutatation of the 3D DCT, because we are going to be giving out immutable borrows.
+        let mut chunked_transform_blocks = Vec::with_capacity(num_chunks);
+        let mut means = Vec::with_capacity(num_chunks);
+
+        for mut chunk in self.values.exact_chunks_mut(chunk_dimensions) {
+            let mean = chunk.mean().unwrap(); // chunk should always be nonempty
+
+            chunk.iter_mut().for_each(|value| *value -= mean); // Softcast specifies a zero-mean distribution
+
+            means.push(mean);
+        }
+
+        // two passes necessary due to the impossibility to coerce 'chunks' in the previous loop to an immutable borrow
+        for (chunk, mean) in self
+            .values
+            .exact_chunks(chunk_dimensions)
+            .into_iter()
+            .zip(means.into_iter())
+        {
+            chunked_transform_blocks.push(ChunkedTransformBlock3D::new(chunk, mean));
+        }
+        chunked_transform_blocks.into_iter()
+    }
+}
+
+pub struct ChunkedTransformBlock3D<'a> {
+    values: ndarray::ArrayView3<'a, f32>,
+    mean: f32,
+}
+
+impl<'a> ChunkedTransformBlock3D<'a> {
+    fn new(values: ndarray::ArrayView3<'a, f32>, mean: f32) -> Self {
+        ChunkedTransformBlock3D { values, mean }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,5 +122,23 @@ mod tests {
             .expect("No DCT performed.");
 
         eprintln!("Got transform_block_3d_dct: {:?}", transform_block_3d_dct);
+    }
+
+    #[test]
+    fn test_print_chunk_means() {
+        let path = "sample-media/sample-5s.mp4";
+        let mut reader = AssetReader::new(path);
+
+        let mut transform_block_3d_dct: TransformBlock3DDCT = reader
+            .pixel_buffer_iter()
+            .macro_block_3d_iterator::<4>()
+            .map(|macro_block| macro_block.y_components)
+            .map(|transform_block| TransformBlock3DDCT::from(transform_block))
+            .next()
+            .expect("No DCT performed.");
+
+        for ChunkedTransformBlock3D { values: _, mean } in transform_block_3d_dct.chunks_iter() {
+            eprintln!("mean:{}", mean);
+        }
     }
 }
