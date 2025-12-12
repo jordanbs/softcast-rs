9a67c13c70107b65e19e548a60874722a4cace90
Jordan Schneider <jbschne@umich.edu>
2025-12-12 11:06:45 -0800

    Organize coding.rs into modules.

--- src/coding.rs
+++ src/coding.rs
@@ -19,123 +19,133 @@ use crate::asset_reader_writer::{transform_block_3d::*, *};
 use ndarray;
 use ndrustfft;
 
-#[derive(Debug)]
-pub struct TransformBlock3DDCT {
-    pub values: ndarray::Array3<f32>,
-}
+mod transform_block_3d_dct {
+    use super::*;
+    use chunked_dct_block::*;
 
-impl<const LENGTH: usize, PixelType: HasPixelComponentType>
-    From<TransformBlock3D<LENGTH, PixelType>> for TransformBlock3DDCT
-{
-    fn from(transform_block: TransformBlock3D<LENGTH, PixelType>) -> Self {
-        let input_values = transform_block.consume_values();
-        let (length, width, height) = input_values.dim();
-        assert_eq!(length, LENGTH);
+    #[derive(Debug)]
+    pub struct TransformBlock3DDCT {
+        pub values: ndarray::Array3<f32>,
+    }
 
-        let mut output_a = input_values;
-        let mut output_b = ndarray::Array3::zeros(output_a.raw_dim());
+    impl<const LENGTH: usize, PixelType: HasPixelComponentType>
+        From<TransformBlock3D<LENGTH, PixelType>> for TransformBlock3DDCT
+    {
+        fn from(transform_block: TransformBlock3D<LENGTH, PixelType>) -> Self {
+            let input_values = transform_block.consume_values();
+            let (length, width, height) = input_values.dim();
+            assert_eq!(length, LENGTH);
 
-        for (axis_idx, axis_len) in [(0, length), (1, width), (2, height)] {
-            let handler =
-                ndrustfft::DctHandler::new(axis_len).normalization(ndrustfft::Normalization::None);
-            ndrustfft::nddct2_par(&output_a, &mut output_b, &handler, axis_idx);
+            let mut output_a = input_values;
+            let mut output_b = ndarray::Array3::zeros(output_a.raw_dim());
 
-            std::mem::swap(&mut output_a, &mut output_b);
+            for (axis_idx, axis_len) in [(0, length), (1, width), (2, height)] {
+                let handler = ndrustfft::DctHandler::new(axis_len)
+                    .normalization(ndrustfft::Normalization::None);
+                ndrustfft::nddct2_par(&output_a, &mut output_b, &handler, axis_idx);
+
+                std::mem::swap(&mut output_a, &mut output_b);
+            }
+
+            TransformBlock3DDCT { values: output_a }
+        }
+    }
+
+    fn max_factor_at_or_below(limit: usize, value: usize) -> usize {
+        assert!(limit > 0);
+        (1..=limit).rev().find(|i| value % i == 0).unwrap()
+    }
+
+    impl<const LENGTH: usize, PixelType: HasPixelComponentType>
+        transform_block_3d::TransformBlock3D<LENGTH, PixelType>
+    {
+        pub fn into_dct(self) -> TransformBlock3DDCT {
+            TransformBlock3DDCT::from(self)
+        }
+    }
+
+    impl<const LENGTH: usize, PixelType: HasPixelComponentType> From<TransformBlock3DDCT>
+        for transform_block_3d::TransformBlock3D<LENGTH, PixelType>
+    {
+        fn from(transform_block_dct: TransformBlock3DDCT) -> Self {
+            let dct_values = transform_block_dct.consume_values();
+            let (length, width, height) = dct_values.dim();
+            assert_eq!(length, LENGTH);
+
+            let mut output_a = dct_values;
+            let mut output_b = ndarray::Array3::zeros(output_a.raw_dim());
+
+            for (axis_idx, axis_len) in [(0, length), (1, width), (2, height)] {
+                let handler = ndrustfft::DctHandler::new(axis_len)
+                    .normalization(ndrustfft::Normalization::None);
+                ndrustfft::nddct3_par(&output_a, &mut output_b, &handler, axis_idx); // dct3 is the inverse of dct2
+
+                std::mem::swap(&mut output_a, &mut output_b);
+            }
+
+            let scale = 0.125 * (length * width * height) as f32; //  dimensions / (2^num_dimensions)
+            output_a.mapv_inplace(|value| value / scale);
+
+            transform_block_3d::TransformBlock3D::with_values(output_a)
+        }
+    }
+
+    impl TransformBlock3DDCT {
+        pub fn consume_values(self) -> ndarray::Array3<f32> {
+            self.values
         }
 
-        TransformBlock3DDCT { values: output_a }
-    }
-}
+        pub fn chunks_iter(&mut self) -> impl Iterator<Item = ChunkedDCTBlock<'_>> {
+            const SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS: (usize, usize, usize) = (1, 44, 30);
+            let (length, width, height) = self.values.dim();
+            let chunk_length =
+                max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.0, length);
+            let chunk_width =
+                max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.1, width);
+            let chunk_height =
+                max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.2, height);
 
-fn max_factor_at_or_below(limit: usize, value: usize) -> usize {
-    assert!(limit > 0);
-    (1..=limit).rev().find(|i| value % i == 0).unwrap()
-}
+            let chunk_size = chunk_width * chunk_height * chunk_length;
+            assert!(chunk_size > 0);
+            assert_eq!((length * width * height) % chunk_size, 0);
+            let num_chunks = (length * width * height) / chunk_size;
 
-impl<const LENGTH: usize, PixelType: HasPixelComponentType>
-    transform_block_3d::TransformBlock3D<LENGTH, PixelType>
-{
-    pub fn into_dct(self) -> TransformBlock3DDCT {
-        TransformBlock3DDCT::from(self)
-    }
-}
+            let chunk_dimensions = (chunk_length, chunk_width, chunk_height);
 
-impl<const LENGTH: usize, PixelType: HasPixelComponentType> From<TransformBlock3DDCT>
-    for transform_block_3d::TransformBlock3D<LENGTH, PixelType>
-{
-    fn from(transform_block_dct: TransformBlock3DDCT) -> Self {
-        let dct_values = transform_block_dct.consume_values();
-        let (length, width, height) = dct_values.dim();
-        assert_eq!(length, LENGTH);
+            // must preflight mutatation of the 3D DCT, because we are going to be giving out immutable borrows.
+            let mut means = Vec::with_capacity(num_chunks);
 
-        let mut output_a = dct_values;
-        let mut output_b = ndarray::Array3::zeros(output_a.raw_dim());
+            for mut chunk in self.values.exact_chunks_mut(chunk_dimensions) {
+                let mean = chunk.mean().unwrap(); // chunk should always be nonempty
 
-        for (axis_idx, axis_len) in [(0, length), (1, width), (2, height)] {
-            let handler =
-                ndrustfft::DctHandler::new(axis_len).normalization(ndrustfft::Normalization::None);
-            ndrustfft::nddct3_par(&output_a, &mut output_b, &handler, axis_idx); // dct3 is the inverse of dct2
+                chunk.iter_mut().for_each(|value| *value -= mean); // Softcast specifies a zero-mean distribution
 
-            std::mem::swap(&mut output_a, &mut output_b);
+                means.push(mean);
+            }
+
+            // two passes necessary due to the impossibility to coerce 'chunks' in the previous loop to an immutable borrow
+            let chunked_transform_blocks = self
+                .values
+                .exact_chunks(chunk_dimensions)
+                .into_iter()
+                .zip(means.into_iter())
+                .map(|(chunk, mean)| ChunkedDCTBlock::new(chunk, mean));
+
+            chunked_transform_blocks.into_iter()
         }
-
-        let scale = 0.125 * (length * width * height) as f32; //  dimensions / (2^num_dimensions)
-        output_a.mapv_inplace(|value| value / scale);
-
-        transform_block_3d::TransformBlock3D::with_values(output_a)
     }
 }
 
-impl TransformBlock3DDCT {
-    pub fn consume_values(self) -> ndarray::Array3<f32> {
-        self.values
+mod chunked_dct_block {
+    pub struct ChunkedDCTBlock<'a> {
+        pub values: ndarray::ArrayView3<'a, f32>,
+        pub mean: f32,
     }
 
-    pub fn chunks_iter(&mut self) -> impl Iterator<Item = ChunkedTransformBlock3D<'_>> {
-        const SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS: (usize, usize, usize) = (1, 44, 30);
-        let (length, width, height) = self.values.dim();
-        let chunk_length = max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.0, length);
-        let chunk_width = max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.1, width);
-        let chunk_height = max_factor_at_or_below(SOFTCAST_RECOMMENDED_CHUNK_DIMENSIONS.2, height);
-
-        let chunk_size = chunk_width * chunk_height * chunk_length;
-        assert!(chunk_size > 0);
-        assert_eq!((length * width * height) % chunk_size, 0);
-        let num_chunks = (length * width * height) / chunk_size;
-
-        let chunk_dimensions = (chunk_length, chunk_width, chunk_height);
-
-        // must preflight mutatation of the 3D DCT, because we are going to be giving out immutable borrows.
-        let mut means = Vec::with_capacity(num_chunks);
-
-        for mut chunk in self.values.exact_chunks_mut(chunk_dimensions) {
-            let mean = chunk.mean().unwrap(); // chunk should always be nonempty
-
-            chunk.iter_mut().for_each(|value| *value -= mean); // Softcast specifies a zero-mean distribution
-
-            means.push(mean);
+    impl<'a> ChunkedDCTBlock<'a> {
+        pub(super) fn new(values: ndarray::ArrayView3<'a, f32>, mean: f32) -> Self {
+            ChunkedDCTBlock { values, mean }
         }
-
-        // two passes necessary due to the impossibility to coerce 'chunks' in the previous loop to an immutable borrow
-        let chunked_transform_blocks = self
-            .values
-            .exact_chunks(chunk_dimensions)
-            .into_iter()
-            .zip(means.into_iter())
-            .map(|(chunk, mean)| ChunkedTransformBlock3D::new(chunk, mean));
-
-        chunked_transform_blocks.into_iter()
-    }
-}
-
-pub struct ChunkedTransformBlock3D<'a> {
-    values: ndarray::ArrayView3<'a, f32>,
-    mean: f32,
-}
-
-impl<'a> ChunkedTransformBlock3D<'a> {
-    fn new(values: ndarray::ArrayView3<'a, f32>, mean: f32) -> Self {
-        ChunkedTransformBlock3D { values, mean }
     }
 }
 
@@ -144,9 +154,11 @@ mod tests {
     use super::*;
     use asset_reader::*;
     use asset_writer::*;
+    use chunked_dct_block::*;
     use pixel_buffer::*;
     use std::fs;
     use std::path;
+    use transform_block_3d_dct::*;
 
     #[test]
     fn test_print_3d_dct() {
@@ -176,7 +188,7 @@ mod tests {
             .next()
             .expect("No DCT performed.");
 
-        for ChunkedTransformBlock3D { values: _, mean } in transform_block_3d_dct.chunks_iter() {
+        for ChunkedDCTBlock { values: _, mean } in transform_block_3d_dct.chunks_iter() {
             eprintln!("mean:{}", mean);
         }
     }
