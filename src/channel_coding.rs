89ac63d54d30e8f21303e8e4777e9c1849fbf5e0
Jordan Schneider <jbschne@umich.edu>
2026-01-05 15:12:02 -0800

    Add ofdm_flexframe test.

--- src/channel_coding.rs
+++ src/channel_coding.rs
@@ -17,20 +17,53 @@
 
 #[cfg(test)]
 mod tests {
+    use num_complex::Complex32;
+
     #[test]
-    fn link_liquid_dsp() {
+    fn ofdm_flexframe() {
         use liquid_dsp_sys;
 
         unsafe {
-            let ofdm_frame = liquid_dsp_sys::ofdmflexframegen_create(
+            let fg = liquid_dsp_sys::ofdmflexframegen_create(
                 64,
                 16,
                 4,
                 std::ptr::null_mut(),
                 std::ptr::null_mut(),
             );
+            assert!(!fg.is_null());
 
-            eprintln!("{:?}", ofdm_frame);
+            let header = [0u8; 8];
+            let payload = [0u8; 20];
+            let mut buf = [Complex32::ZERO; 120];
+
+            let status = liquid_dsp_sys::ofdmflexframegen_assemble(
+                fg,
+                header.as_ptr(),
+                payload.as_ptr(),
+                payload.len().try_into().unwrap(),
+            );
+            assert_eq!(status, 0);
+
+            let status = liquid_dsp_sys::ofdmflexframegen_print(fg);
+            assert_eq!(status, 0);
+
+            let mut frame_complete = 0;
+            while 0 == frame_complete {
+                frame_complete = liquid_dsp_sys::ofdmflexframegen_write(
+                    fg,
+                    buf.as_mut_ptr(),
+                    buf.len().try_into().unwrap(),
+                );
+
+                eprintln!(
+                    "ofdmflexframegen wrote to buffer{}",
+                    match frame_complete {
+                        0 => "",
+                        _ => " (frame complete)",
+                    },
+                );
+            }
         }
     }
 }
