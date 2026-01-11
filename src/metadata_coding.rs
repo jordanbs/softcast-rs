760b66a7b030783c742e802fe436075265f9878f
Jordan Schneider <jbschne@umich.edu>
2026-01-11 12:32:11 -0800

    compress_metadata now takes an iterator as input, and decompress_metadata returns an iterator as output.

--- src/metadata_coding.rs
+++ src/metadata_coding.rs
@@ -15,58 +15,77 @@
 // You should have received a copy of the GNU General Public License along with
 // softcast-rs. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::asset_reader_writer::HasPixelComponentType;
-use crate::channel_coding::slice::*;
 use crate::source_coding::chunk::*;
+use std::io::Read;
 use zstd;
 
 // TODO: compress bitmap of discarded chunks with RLE and huffman
 // TODO: consider using protobuf or similar for metadata binary format
 
-// No iterator here, since we don't want to hold a borrow on the slices or any
-// intermediary arrays.
-pub fn compress_metadata<'a, const DCT_LENGTH: usize, PixelType: HasPixelComponentType>(
-    slices: &[Slice<'_, DCT_LENGTH, PixelType>],
-) -> Result<Box<[u8]>, Box<dyn std::error::Error>> {
-    let mut metadata = Vec::with_capacity(2 * slices.len());
-    slices
-        .iter()
-        .map(|slice| slice.chunk_metadata.mean.to_be_bytes())
-        .flatten()
-        .for_each(|byte| metadata.push(byte));
-    slices
-        .iter()
-        .map(|slice| slice.chunk_metadata.energy.to_be_bytes())
-        .flatten()
-        .for_each(|byte| metadata.push(byte));
+trait ToBytes {
+    fn to_bytes(&self) -> [u8; 8];
+}
 
-    let compressed_metadata = zstd::stream::encode_all(std::io::Cursor::new(metadata), 0)?;
+trait ConstantByteSize {
+    const CONSTANT_BYTE_SIZE: usize;
+}
+
+impl ConstantByteSize for ChunkMetadata {
+    const CONSTANT_BYTE_SIZE: usize = 8;
+}
+
+impl From<&ChunkMetadata> for [u8; ChunkMetadata::CONSTANT_BYTE_SIZE] {
+    fn from(chunk: &ChunkMetadata) -> Self {
+        let mut bytes = [0u8; ChunkMetadata::CONSTANT_BYTE_SIZE];
+        bytes[0..4].copy_from_slice(&chunk.mean.to_be_bytes());
+        bytes[4..8].copy_from_slice(&chunk.energy.to_be_bytes());
+        bytes
+    }
+}
+
+impl From<&[u8; ChunkMetadata::CONSTANT_BYTE_SIZE]> for ChunkMetadata {
+    fn from(bytes: &[u8; ChunkMetadata::CONSTANT_BYTE_SIZE]) -> Self {
+        ChunkMetadata {
+            mean: f32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]),
+            energy: f32::from_be_bytes([bytes[4], bytes[5], bytes[6], bytes[7]]),
+        }
+    }
+}
+
+impl ToBytes for ChunkMetadata {
+    fn to_bytes(&self) -> [u8; 8] {
+        self.into()
+    }
+}
+
+pub fn compress_metadata<'a, I: Iterator<Item = &'a ChunkMetadata>>(
+    chunk_metadata_iter: I,
+) -> Result<Box<[u8]>, Box<dyn std::error::Error>> {
+    let binary_metadata: Box<[u8]> = chunk_metadata_iter
+        .flat_map(|metadata| metadata.to_bytes())
+        .collect();
+
+    // Could stream this, but this buffer should only be on the order of 1-2MB.
+    let compressed_metadata = zstd::stream::encode_all(std::io::Cursor::new(binary_metadata), 0)?;
 
     Ok(compressed_metadata.into())
 }
 
 pub fn decompress_metadata(
-    compressed_metadata: Box<[u8]>,
-) -> Result<Box<[ChunkMetadata]>, Box<dyn std::error::Error>> {
-    let data = zstd::stream::decode_all(std::io::Cursor::new(compressed_metadata))?;
-    assert_eq!(data.len() % size_of::<f32>(), 0); // divisible by 4
+    compressed_metadata: &[u8],
+) -> Result<impl Iterator<Item = ChunkMetadata>, Box<dyn std::error::Error>> {
+    let cursor = std::io::Cursor::new(compressed_metadata);
+    let mut decoder = zstd::stream::read::Decoder::new(cursor)?;
 
-    // could get rid of this copy
-    let floats: Vec<f32> = data
-        .chunks_exact(size_of::<f32>())
-        .map(|bytes| f32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]))
-        .collect();
+    // TODO: has no size hint.
+    let iter = std::iter::from_fn(move || {
+        let mut buf = [0u8; ChunkMetadata::CONSTANT_BYTE_SIZE];
+        decoder.read_exact(&mut buf).ok()?; // handles EOF
 
-    let metadata_len = floats.len() / 2;
-
-    let (means, energies) = floats.split_at(metadata_len);
-    let metadata = means
-        .iter()
-        .zip(energies.iter())
-        .map(|(&mean, &energy)| ChunkMetadata { mean, energy })
-        .collect();
-
-    Ok(metadata)
+        let meta = ChunkMetadata::from(&buf);
+        Some(meta)
+    });
+    Ok(iter)
 }
 
 #[cfg(test)]
@@ -74,6 +93,7 @@ mod tests {
     use super::*;
     use crate::asset_reader_writer::asset_reader::*;
     use crate::asset_reader_writer::pixel_buffer::*;
+    use crate::channel_coding::slice::ChunkIterExt;
 
     #[test]
     fn test_reader_to_slice_metadata_inverse_equality() {
@@ -89,14 +109,17 @@ mod tests {
         let mut y_dct = macro_block.y_components.into_dct();
 
         let y_slices: Box<_> = y_dct.chunks_iter().into_slice_iter(LENGTH).collect();
-        let y_compressed_metadata = compress_metadata(&y_slices).expect("y_metadata failed");
+        let y_metadata_iter = y_slices.iter().map(|slice| &slice.chunk_metadata);
+        let y_compressed_metadata = compress_metadata(y_metadata_iter).expect("y_metadata failed");
         eprintln!(
             "orig_size:{} compressed size:{}",
             y_slices.len() * 2 * 4,
             y_compressed_metadata.len()
         );
-        let y_decompressed_metadata =
-            decompress_metadata(y_compressed_metadata).expect("y_metadata decompression failed");
+        let y_decompressed_metadata: Box<[ChunkMetadata]> =
+            decompress_metadata(&y_compressed_metadata)
+                .expect("y_metadata decompression failed")
+                .collect();
         assert_eq!(y_slices.len(), y_decompressed_metadata.len());
 
         for (y_slice, y_metadata) in y_slices.iter().zip(y_decompressed_metadata.iter()) {
