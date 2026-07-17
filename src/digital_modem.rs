e3007c13c6cfd428b1a684f5e7bd7c39bce58c6d
Jordan Schneider <jbschne@umich.edu>
2026-07-17 09:21:28 -0400

    Use QAM64 for digital modem.

--- src/digital_modem.rs
+++ src/digital_modem.rs
@@ -37,7 +37,7 @@ impl<R: std::io::Read> From<R> for DigitalEncoder<R> {
             check: liquid_sys::crc_scheme_LIQUID_CRC_32,
             fec0: liquid_sys::fec_scheme_LIQUID_FEC_RS_M8_50,
             fec1: liquid_sys::fec_scheme_LIQUID_FEC_NONE,
-            mod_scheme: liquid_sys::modulation_scheme_LIQUID_MODEM_QAM256,
+            mod_scheme: liquid_sys::modulation_scheme_LIQUID_MODEM_QAM64,
         };
         let _guard = FFTW_PLANNER_LOCK.lock().unwrap();
         let fg = unsafe {
